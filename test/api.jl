bdc3064923b9ad022d73c9d1c5d4f563ea613aed
femtotrader <femto.trader@gmail.com>
2016-04-24 16:31:56 +0200

    Test

--- test/api.jl
+++ test/api.jl
@@ -1,3 +1,19 @@
 using Base.Test
 
-@test 1==2
+import RequestsCache: Session, CachedSession
+#import RequestsCache: create_query, execute
+import RequestsCache: get
+
+#session = Session()
+#session = CachedSession()
+session = CachedSession(cache_name="cache.jld", backend="jld", expire_after=Base.Dates.Day(1))
+#println(session)
+
+#prepared_query = create_query("GET", "http://httpbin.org/get", query = Dict("title" => "page1"))
+#response = execute(prepared_query; session=session)
+
+response = get(session, "http://httpbin.org/get"; query = Dict("title" => "page1"))
+
+parsed_resp = Requests.json(response)
+@test parsed_resp["args"]["title"] == "page1"
+@test parsed_resp["headers"]["Host"] == "httpbin.org"
